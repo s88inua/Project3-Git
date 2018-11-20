fd692d2c07abeadb8f371a5cde2d662531466dc1
S88inua <s88inua@gmail.com>
2018-11-20 14:35:06 +0200

    LoftSchool Домашнее задание №3

---
+++ index2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Me
+ * Date: 19/11/18
+ * Time: 19:46
+ */
+require ('./function.php');
+task2();
---
+++ index4.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Me
+ * Date: 19/11/18
+ * Time: 18:33
+ */
+require ('./function.php');
+task4();
